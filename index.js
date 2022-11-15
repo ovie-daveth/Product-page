4bd8d80969e63075e8f6d0a450e3fe68fb500e03
ovie-daveth <105714886+ovie-daveth@users.noreply.github.com>
2022-11-15 21:28:34 +0100

    updated java and css

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-let cartItem = document.getElementById("cart")
-let cartIcon = document.getElementById("cart-icon")
-let cartEl = document.querySelector(".cart")
-let count = 0
-cartItem.textContent = count
+let cartItem = document.getElementById("cart");
+let cartIcon = document.getElementById("cart-icon");
+let cartEl = document.querySelector(".cart");
+let costEl = document.getElementById("cost");
+let quantityEl = document.getElementById("quantity");
+
+let count = 0;
+let amount = 0;
+cartItem.textContent = count;
+costEl.textContent = amount;
+quantityEl.textContent = count;
 
 function addToCart(){
-    count = count + 1
-    cartItem.textContent = count
+    count = count + 1;
+    amount += 200;
+
+
+    cartItem.textContent = count;
+    costEl.textContent = amount;
+    quantityEl.textContent = count;
 }
 
 cartIcon.addEventListener("click", () => {
